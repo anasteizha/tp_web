ea88be7af5553870cbca25e18391ae1107066012
Kuznetsova Nastya <kn230902@gmail.com>
2022-04-14 23:30:29 +0300

    fix generation

--- askme/generate.py
+++ askme/generate.py
@@ -15,6 +15,7 @@ def generate_tags(num_records_per_query):
         yield Tag(name=f"tag {i + 1}")
 
 def create_tags():
+    print("Creating tags")
     Tag.objects.bulk_create(generate_tags(10001))
 
 def generate_profiles(num_records_per_query):
@@ -26,6 +27,7 @@ def generate_users():
         yield User(username=f'user_{i}')
 
 def create_profiles():
+    print("Creating profiles")
     User.objects.bulk_create(generate_users())
 
     Profile.objects.bulk_create(generate_profiles(10001))
@@ -35,7 +37,8 @@ def generate_rating_question(num_records_per_query, count):
         yield RatingQuestion(grade=1, question_id=(num_records_per_query * count + i) % 100010 + 1, profile_id=i + 1)
 
 def create_rating_question():
-    for i in range(0, 2000000, 10001):
+    print("Creating ratings questions")
+    for i in range(0, 1000000, 10001):
         RatingQuestion.objects.bulk_create(generate_rating_question(10001, i // 10001))
 
 def generate_rating_answer(num_records_per_query, count):
@@ -43,7 +46,8 @@ def generate_rating_answer(num_records_per_query, count):
         yield RatingAnswer(grade=1, answer_id=(num_records_per_query * count + i) % 1000100 + 1, profile_id=i + 1)
 
 def create_rating_answer():
-    for i in range(0, 2000000, 10001):
+    print("Creating ratings answers")
+    for i in range(0, 1000000, 10001):
         RatingAnswer.objects.bulk_create(generate_rating_answer(10001, i // 10001))
 
 def generate_answers(num_records_per_query, count):
@@ -53,6 +57,7 @@ def generate_answers(num_records_per_query, count):
         question_id=(num_records_per_query * count + i) % 100010 + 1)
 
 def create_answers():
+    print("Creating answers")
     for i in range(0, 1000000, 10001):
         Answer.objects.bulk_create(generate_answers(10001, i // 10001))
 
@@ -62,18 +67,14 @@ def generate_questions(num_records_per_query, count):
         text=f"This is text for question №{num_records_per_query * count + i + 1}",
         profile_id=i + 1)
 
-def change_questions(count):
-    for i in range(10001):
-        #Question.objects.all().get(id=i + 1).tags.add(Tag.objects.get(id=i % 9999 + 3))
-        #Question.objects.all().get(id=i + 1).tags.add(Tag.objects.get(id=i % 10000 + 2))
-        yield Question.objects.all().get(id=10001 * count + i + 1).tags.add(Tag.objects.get(id=i + 1))
-
 def create_questions():
     for i in range(0, 100000, 10001):
         Question.objects.bulk_create(generate_questions(10001, i // 10001))
 
-    for i in range(0, 100000, 10001):
-        Question.objects.bulk_update(change_questions(i // 10001), ['tags'])
+    for i in range(100010):
+        #Question.objects.all().get(id=i + 1).tags.add(Tag.objects.get(id=i % 9999 + 3))
+        #Question.objects.all().get(id=i + 1).tags.add(Tag.objects.get(id=i % 10000 + 2))
+        Question.objects.get(id=i + 1).tags.add(Tag.objects.get(id=i % 10001 + 1))
 
 def count_rating_questions():
     Question.objects.annotate(rating=Sum('RatingQuestion__grade'))
